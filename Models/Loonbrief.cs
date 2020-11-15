0afa63bd32edb847f5e8c6f51fd6283975962e23
Mickeyke <michael.carels@live.be>
2020-11-15 20:17:12 +0100

    update loonbrief

--- Models/Loonbrief.cs
+++ Models/Loonbrief.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text.Json;
 using System.Text.RegularExpressions;
+using System.Xml.Serialization;
 
 namespace GeneratieServiceAPI.Models
 {
@@ -10,8 +11,15 @@ namespace GeneratieServiceAPI.Models
     {
         private string _name;
         private string _lastName;
+        private string _street;
+        private string _number;
         private string _city;
-        private int _postalCode;
+        private string _postalCode;
+        //private int _postalCode;
+        private string _status; 
+        private string _dependents;
+
+        [XmlAttribute("Id")]
         public Guid Id { get; set; }
         //elke parameter toevoegen dat moet verwerkt worden. 
         public string Name
@@ -25,7 +33,7 @@ namespace GeneratieServiceAPI.Models
                 }
                 else
                 {
-                    throw new ArgumentException("No correct Value");
+                    throw new ArgumentException("No correct value.");
                 }
             }
         }
@@ -40,14 +48,43 @@ namespace GeneratieServiceAPI.Models
                 }
                 else
                 {
+                    throw new ArgumentException("No correct value.");
+                }
+            }
+        }
+        public string Registerkey { get; set; }//00.00.00 - 000.00 dit is de vorm dit het zou moeten hebben  lengte is 14 karakter waarvan 2,5,12 een Punt en 8 - 
+
+
+        public string Street { get { return _street; }
+            set
+            {
+                if (Regex.IsMatch(value, @"^\p{Lu}\p{Ll}*$"))
+                {
+                    _street = value;
+                }
+                else
+                {
+                    throw new ArgumentException("No correct Value");
+                }
+            } 
+        }
+        public string Number
+        {
+            get { return _number; }
+            set
+            {
+                if (Regex.IsMatch(value, @"/^(a|A)([0-9])$/"))//check on numbers and 1 letter 
+                {
+                    _number = value;
+                }
+                else
+                {
                     throw new ArgumentException("No correct Value");
                 }
             }
         }
-        public string Registerkey { get; set; }
-        public string Street { get; set; }
-        public int Number { get; set; }
-        public int PostalCode
+        //public int PostalCode
+        public string PostalCode
         {
             get { return _postalCode; }
             set
@@ -59,13 +96,19 @@ namespace GeneratieServiceAPI.Models
                     {
                         foreach (JsonElement el in document.RootElement.EnumerateArray())
                         {
-                            if (el.TryGetProperty("city", out JsonElement result))
+                            if (el.TryGetProperty("zip", out JsonElement result))
                             {
-                                if (Int32.Parse(result.ToString()) == value)
+                                if (result.ToString() == value)
                                 {
                                     _postalCode = value;
+                                    break;
+                                }
+                                else
+                                {
+                                    throw new ArgumentException("Postal code not valid.");
                                 }
                             }
+
                         }
                     }
                 }
@@ -88,14 +131,50 @@ namespace GeneratieServiceAPI.Models
                                 if (result.ToString() == value)
                                 {
                                     _city = value;
+                                    break;
+                                }
+                                else
+                                {
+                                    throw new ArgumentException("City name not valid.");
                                 }
                             }
+
                         }
                     }
                 }
             }
         }
-        public string Status { get; set; } //Burgerlijke staat
-        public List<string> Dependents { get; set; } //Personen ten laste
+        public string Status { get { return _status; }
+            set
+            {
+                //kijken of het enkel bestaat uit tekst en of het een van de geldige value zijn. 
+                if (Regex.IsMatch(value, @"^\p{Lu}\p{Ll}*$") && (value === "Ongehuwd" || value === "Gehuwd" ||value === "Gescheiden" ||value === "Verweduwd"))
+                {
+                    _status = value;
+                }
+                else
+                {
+                    throw new ArgumentException("No correct Value");
+                }
+            } } //Burgerlijke staat : Ongehuwd,Gehuwd,Gescheiden,Verweduwd
+        
+        //public List<string> Dependents { get { return _dependents; }
+        //    set
+        //    {
+        //        //kijken of het enkel bestaat uit tekst en of het een van de geldige value zijn. 
+        //        //enkel nog kijken of het wel van alle waardes in de arry gecontroleerd wordt of het enkel uit letters bestaat. 
+        //        if (Regex.IsMatch(value, @"^\p{Lu}\p{Ll}*$"))
+        //        {
+        //            _dependents = value;
+        //        }
+        //        else
+        //        {
+        //            throw new ArgumentException("No correct Value");
+        //        }
+        //    } } //Personen ten laste
+        //    }
+        //}
+       
+        public string Dependents { get; set; } //Personen ten laste
     }
 }
